436b1d9cc2234a8e67f9eacc22d327ac71e6bd47
raesii <leisolo@gmail.com>
2022-03-26 11:13:17 -0700

    added code to call all employees, single employee and delete employee

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3000;
+const app = inquirer(); // is this ok?
+
+// is express middleware needed?
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 const db = mysql.createConnection(
     {
@@ -10,5 +15,60 @@ const db = mysql.createConnection(
         password: '2xzne4LIeY$#',
         database: 'staff'
     },
-    console.log('Connected to the election database.')
+    console.log('Connected to the staff database.')
 );
+
+// Get all employees
+app.get('/api/employees', (req, res) => {
+    const sql = `SELECT * FROM employees`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// Get a single employee
+app.get('/api/employees/:id', (req, res) => {
+    const sql = `SELECT * FROM employees WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// Delete an employee
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM employees WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
